86b841248bacc5030a7908c0b64219fd3f53da9f
github username <github email address>
2019-07-01 14:04:30 -0400

    swole

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,31 @@
 class Gym
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def my_memberships 
+    Membership.all.select {|membership| membership.gym == self}
+  end
+
+  def my_lifters
+    my_memberships.collect {|ship| ship.lifter}
+  end 
+
+  def my_lifters_names
+    my_lifters.collect {|lifter| lifter.name}
+  end
+
+  def lift_total
+    total = 0
+    my_lifters.each do |lifter|
+      total += lifter.lift_total
+    end
+    total
+  end
+
+
 end
