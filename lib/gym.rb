88e0dadc10d6cb9ae0ddb1c506ff6cd7eb7ee46b
github username <github email address>
2019-07-02 08:40:56 -0400

    got swole

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,38 @@
 class Gym
   attr_reader :name
 
+  @@gyms =[]
   def initialize(name)
     @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@gyms
+  end
+
+  def memberships
+    #lists all the memberships this gym offers
+    Membership.all.select {|membership| membership.gym == self}
+  end
+
+  def members
+    #gets names of all lifters who have memberships at this gym
+    members = []
+    memberships.each do |membership|
+      if membership.gym == self
+        members << membership.lifter
+      end
+    end
+    members
+  end
+
+  def combined_lift_total
+    #gets combined lift total of all lifters at this gym
+    total = 0
+    members.each do |member|
+      total += member.lift_total
+    end
+    total
   end
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,55 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@lifters = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    self.class.all << self
   end
+
+  def self.all
+    @@lifters
+  end
+
+  def memberships
+    #gets a list of all memberships this instance of lifter has
+    Membership.all.select {|membership| membership.lifter == self}
+  end
+
+  def gyms
+    #gets a list of all gyms this instance of lifter has a membership to
+    mygyms = []
+    memberships.each do |membership|
+      if membership.lifter == self
+       mygyms << membership.gym
+      end
+    end
+    mygyms
+  end
+
+  def self.average_lift_total
+    #gets the average lift total of all lifters
+    lift_totals = []
+    self.all.each do |lifter|
+      lift_totals << lifter.lift_total
+    end
+    lift_totals.sum/lift_totals.size
+  end
+
+  def total_membership_cost
+    #total cost of this lifters gym memberships
+    total_cost = []
+    memberships.each do |membership|
+      total_cost << membership.cost
+    end
+    total_cost.sum
+  end
+
+  def sign_me_up(gym, membership_cost)
+    #signs this lifter up for a new gym
+    Membership.new(membership_cost, self, gym)
+  end
+
 end
